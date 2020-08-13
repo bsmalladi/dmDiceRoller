579f236dabfdfb480d4788c17f11bedd002b6058
bmalladi <bmalladi@umich.edu>
2020-08-12 18:56:25 -0700

    default d6 roller

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    numDice: 0,
+    plusMinus: "+",
+    modifier: 0,
+    currentRoll: 0,
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  rollDice = () => {
+    let newRoll = Math.floor(Math.random() * 6) + 1;
+    newRoll =
+      newRoll +
+      (this.state.plusMinus === "+"
+        ? 1 * this.state.modifier
+        : -1 * this.state.modifier);
+    this.setState({ currentRoll: newRoll });
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          name="numDice"
+          type="number"
+          min="0"
+          max="100"
+          value={this.state.numDice}
+          onChange={this.handleChange}
+        />
+        <strong>d6</strong>
+        <input
+          type="radio"
+          id="+"
+          name="plusMinus"
+          value="+"
+          checked={this.state.plusMinus === "+"}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="+">+</label>
+        <input
+          type="radio"
+          id="-"
+          name="plusMinus"
+          value="-"
+          checked={this.state.plusMinus === "-"}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="-">-</label>
+        <input
+          name="modifier"
+          type="number"
+          min="0"
+          max="1000"
+          value={this.state.modifier}
+          onChange={this.handleChange}
+        />
+        <input type="button" onClick={this.rollDice} value="Roll!" />
+        <strong>{this.state.currentRoll}</strong>
+      </form>
+    );
+  }
 }
 
 export default App;
