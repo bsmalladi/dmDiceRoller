5a8f43ae14cb293cb708c031f1472e091b1c5027
bmalladi <bmalladi@umich.edu>
2020-08-17 11:21:48 -0700

    ui to add more custom rolls

--- src/components/customRollers.jsx
+++ src/components/customRollers.jsx
@@ -4,15 +4,85 @@ import SingleCustom from "./singleCustom";
 class CustomRollers extends Component {
   state = {
     custom: [
-      { num: 3, type: 6 },
-      { num: 2, type: 10 },
+      { num: 3, type: 6, pm: "+", mod: 1 },
+      { num: 2, type: 10, pm: "-", mod: 2 },
     ],
     currentRoll: 0,
+    numDice: 0,
+    diceType: 0,
+    plusMinus: "+",
+    modifier: 0,
+  };
+
+  addCustom = () => {
+    const custom = [
+      ...this.state.custom,
+      {
+        num: this.state.numDice,
+        type: this.state.diceType,
+        pm: this.state.plusMinus,
+        mod: this.state.modifier,
+      },
+    ];
+    this.setState({ custom });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   };
 
   render() {
     return (
       <div>
+        <form>
+          <input
+            name="numDice"
+            type="number"
+            min="0"
+            max="100"
+            value={this.state.numDice}
+            onChange={this.handleChange}
+          />
+          <strong>d</strong>
+          <input
+            name="diceType"
+            type="number"
+            min="0"
+            max="100"
+            value={this.state.diceType}
+            onChange={this.handleChange}
+          />
+          <input
+            type="radio"
+            id="+"
+            name="plusMinus"
+            value="+"
+            checked={this.state.plusMinus === "+"}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="+">+</label>
+          <input
+            type="radio"
+            id="-"
+            name="plusMinus"
+            value="-"
+            checked={this.state.plusMinus === "-"}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="-">-</label>
+          <input
+            name="modifier"
+            type="number"
+            min="0"
+            max="1000"
+            value={this.state.modifier}
+            onChange={this.handleChange}
+          />
+          <input type="button" onClick={this.addCustom} value="Add!" />
+          <strong>{this.state.currentRoll}</strong>
+        </form>
         {this.state.custom.map((customRoll) => (
           <SingleCustom num={customRoll.num} dType={customRoll.type} />
         ))}
